50abab898460f3de2aba0b68ebcd8713a3901860
Kishan Raj <kishanrajrby2.com>
2025-08-18 23:02:03 +0530

    delete env file

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,38 @@
 // backend/server.js
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const connectDB = require('./db');
-const employeeRoutes = require('./routes/employeeRoutes');
-const leaveRoutes = require('./routes/leaveRoutes');
+const connectDB = require("./db");
+const employeeRoutes = require("./routes/employeeRoutes");
+const leaveRoutes = require("./routes/leaveRoutes");
 
 const app = express();
-app.use(cors(
-  {
-    origin: process.env.FRONTEND_URL
-  }
-));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(bodyParser.json());
 
 // routes
-app.use('/api/employees', employeeRoutes);
-app.use('/api/leaves', leaveRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/leaves", leaveRoutes);
 
 // simple health
-app.get('/health', (req, res) => res.json({ status: 'ok' }));
+app.get("/health", (req, res) => res.json({ status: "ok" }));
 
 // global error handler
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(err.status || 500).json({ message: err.message || 'Server error' });
+  res
+  .status(err.status || 500)
+  .json({ message: err.message || "Server error" });
 });
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
